57c0f23804d8d80ac04d5a4f569bde47c1891e42
DaVinci <maxkgd@mail.ru>
2023-09-07 16:12:12 +0200

    Сменил модуль на pymysql.

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-import mysql.connector
-from mysql.connector import Error
-from config import host, port, user, password
+import pymysql
+from config import host, user, password, db_name
 
 
 def get_con():
     try:
-        connection = mysql.connector.connect(
+        connection = pymysql.connect(
             host=host,
-            port=port,
             user=user,
-            password=password
+            port=3306,
+            password=password,
+            database=db_name
         )
+        print('Successfully connection...')
+        print('#'*20)
+
+        try:
+            with connection.cursor() as curs:
+                curs.execute('SHOW DATABASES;')
+                print(curs.fetchall())
+        finally:
+            connection.close()
 
     except Exception as ex:
         print(f'Not connection...{ex}')
